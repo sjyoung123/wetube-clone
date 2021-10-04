19e9ce19c0b417cce0cd68881591178cb5d75627
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-04 19:11:41 +0900

    +router&controller&export

---
+++ src/controllers/userController.js
@@ -0,0 +1,4 @@
+export const join = (req,res) => res.send("join");
+
+export const edit = (req,res) => res.send("Edit user");
+export const remove = (req,res) => res.send("Remove user");
--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 import express from "express";
+import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+
+const logger = morgan("dev");
 const app = express();
-
 const PORT = "4000";
 
-const hadleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+app.use(logger);
 
-app.listen(PORT,hadleListening); //ctrl + c => kill the server
+app.use("/",globalRouter);
+app.use("/users",userRouter);
+app.use("/videos",videoRouter);
+
+
+
+const handleListening = () =>{
+    console.log(`server listening on port https://localhost:${PORT}`);
+}
+
+app.listen(PORT,handleListening); //ctrl + c => kill the server
