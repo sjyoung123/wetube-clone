1f0d2bc3f8d7ef7e8d50fe466ab3eec08107f076
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-06 21:42:49 +0900

    Update MVP CSS & Conditional

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 export const join = (req,res) => res.send("Join");
-export const login = (req,rex) => res.send("Log in")
+export const login = (req,res) => res.send("Log in")
 export const edit = (req,res) => res.send("Edit user");
 export const remove = (req,res) => res.send("Remove user");
 export const see = (req,res) => res.send("See user");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,10 @@
-export const trending = (req,res) => res.render("home",{pageTitle:"Home"});
-export const search = (req,res) => res.send("Search video")
+const fakeUser = {
+    username:"codeyoung",
+    loggedIn: true,
+};
+
+export const trending = (req,res) => res.render("home",{pageTitle:"Home",fakeUser,});
+export const search = (req,res) => res.send("Search video");
 export const see = (req,res) => res.render("watch",{pageTitle:"Watch"});
 export const edit = (req,res) => res.render("edit",{pageTitle:"Edit"});
 export const upload = (req,res) => res.send("Upload video");
