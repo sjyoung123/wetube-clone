7fa66368e9312ab478b38bb8f1c710571a7edace
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-08 15:18:32 +0900

    Created mixins file

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,28 @@
-const fakeUser = {
-    username:"codeyoung",
-    loggedIn: true,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "first video",
+      rating: 4,
+      comments: 2,
+      createAt: "2 minutes ago",
+    },
+    {
+      title: "second video",
+      rating: 4,
+      comments: 2,
+      createAt: "2 minutes ago",
+    },
+    {
+      title: "Third video",
+      rating: 4,
+      comments: 2,
+      createAt: "2 minutes ago",
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req,res) => res.render("home",{pageTitle:"Home",fakeUser,});
-export const search = (req,res) => res.send("Search video");
-export const see = (req,res) => res.render("watch",{pageTitle:"Watch"});
-export const edit = (req,res) => res.render("edit",{pageTitle:"Edit"});
-export const upload = (req,res) => res.send("Upload video");
-export const deleteVideo = (req,res) => res.send("Delete video");
+export const search = (req, res) => res.send("Search video");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const upload = (req, res) => res.send("Upload video");
+export const deleteVideo = (req, res) => res.send("Delete video");
--- src/server.js
+++ src/server.js
@@ -5,26 +5,23 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-
 const logger = morgan("dev");
 const app = express();
 const PORT = "4000";
 
 //app.disable("x-powered-by");
 
-app.set("view engine","pug");
+app.set("view engine", "pug");
 app.set("views", "./src/views");
 
 app.use(logger);
 
-app.use("/",globalRouter);
-app.use("/users",userRouter);
-app.use("/videos",videoRouter);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
+const handleListening = () => {
+  console.log(`server listening on port https://localhost:${PORT}`);
+};
 
-
-const handleListening = () =>{
-    console.log(`server listening on port https://localhost:${PORT}`);
-}
-
-app.listen(PORT,handleListening); //ctrl + c => kill the server
+app.listen(PORT, handleListening); //ctrl + c => kill the server
