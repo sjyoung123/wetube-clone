5701916a92860f3b033be7a942d5ed5398cfe219
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-12 18:27:24 +0900

    Updated VideoRouter URL

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,10 +9,9 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-// videoRouter.get("/:id(\\d+)/edit", getEdit);
-// videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
