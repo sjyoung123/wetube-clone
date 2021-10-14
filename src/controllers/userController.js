1de0270f816b6251f0c60febdf167ca04544cdaa
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-14 18:28:41 +0900

    Updated Join(With Hashed Password)
    Hashed Password By bcryptjs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,21 @@
-export const join = (req,res) => res.send("Join");
-export const login = (req,res) => res.send("Log in")
-export const edit = (req,res) => res.send("Edit user");
-export const remove = (req,res) => res.send("Remove user");
-export const see = (req,res) => res.send("See user");
-export const logout = (req,res) => res.send("Log out");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = async (req, res) => {
+  const { email, username, password, name, location } = req.body;
+  await User.create({
+    email,
+    username,
+    password,
+    name,
+    location,
+  });
+  return res.redirect("/login");
+};
+export const login = (req, res) => res.send("Log in");
+export const edit = (req, res) => res.send("Edit user");
+export const remove = (req, res) => res.send("Remove user");
+export const see = (req, res) => res.send("See user");
+export const logout = (req, res) => res.send("Log out");
