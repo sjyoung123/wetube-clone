57e0a02ef11bfbf3358cb9b2c337eb7868461ab2
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-20 21:00:26 +0900

    Fixed a bug
    hash only when change password
    by isModified()

--- src/middleware.js
+++ src/middleware.js
@@ -2,15 +2,12 @@ import multer from "multer";
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  //   if (req.session.loggedIn) {
-  //     res.locals.loggedIn = true;
-  //   }
+
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
-  //console.log(res.locals);
+
   next();
 };
-//res.locals => 모든 템플릿에서 공유한다.
 
 export const loggedInOnlyMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
--- src/models/User.js
+++ src/models/User.js
@@ -15,9 +15,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  console.log("User Password", this.password);
-  this.password = await bcrypt.hash(this.password, 5);
-  console.log("Hashed Password", this.password);
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
