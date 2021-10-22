9abb5ebe4065754c1367a34ad7e2bc6672fa8205
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-22 18:40:26 +0900

    Updated css part2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -119,7 +119,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(`${keyword}`, "i"),
       },
-    });
+    }).populate("owner");
   }
 
   return res.render("videos/search", { pageTitle: "Search", videos });
