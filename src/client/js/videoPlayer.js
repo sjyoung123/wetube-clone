3e764c70714142cf66eb9b4793d8efc57ddd42f7
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-23 15:15:19 +0900

    Updated video playBtn

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,20 @@
-console.log("cool");
+const video = document.getElementById("video");
+
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayBtnClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handlePlay = (e) => (playBtn.innerText = "Pause");
+const handlePause = (e) => (playBtn.innerText = "Play");
+
+playBtn.addEventListener("click", handlePlayBtnClick);
+video.addEventListener("play", handlePlay);
+video.addEventListener("pause", handlePause);
