6ef97dc160f7389fc527446114c99fbc3f5654a1
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-23 16:58:06 +0900

    Updated video volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,7 +3,7 @@ const video = document.getElementById("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayBtnClick = (e) => {
   if (video.paused) {
@@ -11,10 +11,58 @@ const handlePlayBtnClick = (e) => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handlePlay = (e) => (playBtn.innerText = "Pause");
-const handlePause = (e) => (playBtn.innerText = "Play");
+
+// const handleMuteBtnClick = (e) => {
+//   if (video.muted) {
+//     video.muted = false;
+//   } else {
+//     video.muted = true;
+//   }
+//   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+//   volumeRange.value = video.muted ? 0 : 0.5;
+//   video.volume = volumeRange.value;
+// };
+
+// const handleVolumeRange = (e) => {
+//   video.volume = volumeRange.value;
+//   if (volumeRange.value == 0) {
+//     muteBtn.innerText = "Unmute";
+//   } else {
+//     muteBtn.innerText = "Mute";
+//   }
+// };
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+const handleMuteBtnClick = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+const handleVolumeRange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+
+  volumeValue = Number(value);
+  video.volume = value;
+
+  if (volumeValue === 0) {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
+  }
+};
 
 playBtn.addEventListener("click", handlePlayBtnClick);
-video.addEventListener("play", handlePlay);
-video.addEventListener("pause", handlePause);
+muteBtn.addEventListener("click", handleMuteBtnClick);
+volumeRange.addEventListener("input", handleVolumeRange);
