c6e0d8a5e2fec27d8d0a93d786b6f31c96e47c1b
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-05 16:19:30 +0900

    Updated realtime comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,15 @@
-import { async } from "regenerator-runtime";
+// import { async } from "regenerator-runtime";
 
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
+
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  newComment.innerText = `${text}`;
+  videoComments.prepend(newComment);
+};
 
 const handleSubmit = async (event) => {
   event.preventDefault();
@@ -11,7 +19,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,6 +27,9 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
+  if (status === 201) {
+    addComment(text);
+  }
 };
 if (form) {
   form.addEventListener("submit", handleSubmit);
