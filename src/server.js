2882a1a8a353aa08fb408d123df2816cfecf7c97
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-12-18 21:12:02 +0900

    bug

--- src/server.js
+++ src/server.js
@@ -33,6 +33,7 @@ app.use(
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
+
   next();
 });
 
