fdee7f6151945aec42c24e91ee3e57c2ad8d34a4
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-12-18 21:32:44 +0900

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -13,7 +13,7 @@ const multerUploader = multerS3({
   s3: s3,
   bucket: "mywetubee",
   acl: "public-read",
-  contentType: multerS3.AUTO_CONTENT_TYPE,
+  // contentType: multerS3.AUTO_CONTENT_TYPE,
 });
 
 export const localsMiddleware = (req, res, next) => {
